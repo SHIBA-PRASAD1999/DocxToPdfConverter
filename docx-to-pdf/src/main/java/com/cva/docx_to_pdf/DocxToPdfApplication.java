d3d2149e481bcdd56e087bcdd23a615d7c57babb
Priyabrata <sahooshibaprasad291@gmail.com>
2025-04-10 21:37:25 +0530

    maven setup inserted

---
+++ docx-to-pdf/src/main/java/com/cva/docx_to_pdf/DocxToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.cva.docx_to_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocxToPdfApplication.class, args);
+	}
+
+}
---
+++ docx-to-pdf/src/test/java/com/cva/docx_to_pdf/DocxToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cva.docx_to_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
